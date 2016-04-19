d81762c24cb9c67a01335c603b69d91fb5291ad9
Yassine Ragnar <yassinemorsaoui@gmail.com>
2016-04-19 23:18:53 +0100

    adding database

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 var express=require('express');
 var PORT=process.env.PORT || 3000;
 var server=express();
-middleware=require('./middleware');
+var middleware=require('./middleware');
+var db=require('./database');
 
 server.use(middleware.logger);
 server.get('/',function(req,res){
 	res.send('Connection OK');
 });
-server.get('/Trains',middleware.requireAuthentification ,function(req,res){
-	res.send('Morroco Trains');
+server.get('/Trips',middleware.requireAuthentification ,function(req,res){
+		if(req.query.start && req.query.end){
+            console.dir(req.query.start);console.dir(req.query.end);
+            findCustomeTrip(req,res,req.query.start,req.query.end);
+        }
+        else 
+			res.send('Morroco Trains');
 })
-server.listen(PORT);
 
+function findCustomeTrip(req,res,start,end){
+    db.find('Trips', {"Start.Name":start,"End.Name":end}, function (err,  
+  resources) {
+  res.json(resources);
+  });
+}
+
+//
+server.listen(PORT);
+//
