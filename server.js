84b2d40b614bd6ce5aa8e4446c7c9d8adff933de
Yassine Ragnar <yassinemorsaoui@gmail.com>
2016-04-19 14:06:59 +0100

    refactor middleware

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 var express=require('express');
 var PORT=3000;
 var server=express();
-var middleware={
-	requireAuthentification:function(req, res, next){
-		console.log('private route');
-		next();
-	},
-	logger:function(req, res, next){
-		console.log(req.method+' '+req.originalUrl);
-		next();
-	}
-}
+middleware=require('./middleware');
 
 server.use(middleware.logger);
 server.get('/',function(req,res){
